d3e65faa59f93eaca3bd51dfb1961b4940e9e191
xoflo <xordelish@gmail.com>
2025-07-10 20:22:35 +0800

    July 10

--- api_video.php
+++ api_video.php
@@ -1,5 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 if (isset($_FILES['file'])) {
     echo "File received: " . $_FILES['file']['name'] . "\n";
